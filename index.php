987becb5644b74316d5062025e058aac3e150f92
Dawid Szafrański <73634614+szafranski-dawid@users.noreply.github.com>
2020-11-05 08:29:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
 <?php
-echo 'Jestem królem'
+
+$conn = new mysqli("","","","");
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+
 ?>
+</body>
+</html>
