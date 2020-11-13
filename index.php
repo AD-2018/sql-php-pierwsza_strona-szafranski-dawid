5c49c325c1b04492572897bdcd9065a13a8e7398
Dawid Szafrański <73634614+szafranski-dawid@users.noreply.github.com>
2020-11-13 10:49:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,27 @@
 <!DOCTYPE html>
-<html>
 <head>
-<link rel="stylesheet" href="style.css">
-<h1>kox</h1>
+<link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body>
 <?php
-$servername = "serwer";
-$username = "użytkownik";
-$password = "haslo";
-$dbname = "baza";
-
-$conn= new mysqli($servername,$username,$password,$dbname);
-
-$sql ="select * from pracownicy";
-echo("<h3>kozak</h3>");
+$servername = "mysql-gruby.alwaysdata.net";
+$username = "gruby";
+$password = "Marekglupek123";
+$dbname = "gruby_dawidek";
+ 
+$conn = new mysqli($servername, $username, $password, $dbname);
+ 
+$sql= "SELECT * FROM pracownicy";
+echo("<h3>zadanie </h3>");
+echo("<li>".$sql."<br><br>");
 $result = mysqli_query($conn, $sql);
-
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>id_pracownicy</th><th>imie</th><th>dzial</th><th>zarobki</th><th>data_urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)){
-echo("<tr>");
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
-    echo("<tr>");
-echo("</tr>");
+echo('<table border="1" class="tabelka_moja">');
+while($row = mysqli_fetch_assoc($result) ) {
+   echo('<tr>');
+   echo("<td>".$row['imie']."</td><td>".$row['zarobki']."</td><td>".$row['data_urodzenia']."</td>");
+   echo('</tr>');
 }
 echo('</table>');
 ?>
-
-
 </body>
 </html>
