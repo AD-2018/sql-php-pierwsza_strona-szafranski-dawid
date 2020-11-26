8f5fa69257cc5d00d5a80d8a9d830139d856120a
Dawid Szafrański <73634614+szafranski-dawid@users.noreply.github.com>
2020-11-26 08:31:41 +0100

    Update nieobecnosci.php

--- nieobecnosci.php
+++ nieobecnosci.php
@@ -12,15 +12,23 @@
     <br>
     <br>
     <div class="nav">
-         <a href="index.php">Strona Główna</a>  
+         <a href="index.php">Strona Główna</a> 
+         <br>        
          <a href="pracownicy.php">Pracownicy</a>
+         <br>
          <a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>   
+         <br>
          <a href="funkcje_agregujace.php">Funkcje Agregujace</a>  
+         <br>
          <a href="data_czas.php">Data i Czas</a>
+         <br>
          <a href="nieobecnosci.php">Nieobecności Pracowników</a>
+         <br>
          <a href="strona.php">Strona</a>
+         <br>
          <a href="daneDoBazy.html">Dane Do Bazy</a>
-         <a href="insert.php">insert.php</a>
+         <br>
+	 <a href="insert.php">insert.php</a>
     </div>
     <br>
 </nav>  
