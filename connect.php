6fb7df8d8facddbde9ded7314f5385bb2b7cb913
Dawid Szafrański <73634614+szafranski-dawid@users.noreply.github.com>
2020-11-26 08:18:19 +0100

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "mysql-kcz.alwaysdata.net";
+$username = "kcz";
+$password = "zaq1@WSX";
+$dbname = "kcz_20";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
