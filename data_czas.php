556dd3c8c45fd1c88616fe1c45baf82c5553ce06
Dawid Szafrański <73634614+szafranski-dawid@users.noreply.github.com>
2020-11-26 08:34:47 +0100

    Update data_czas.php

--- data_czas.php
+++ data_czas.php
@@ -22,7 +22,7 @@
          <br>
          <a href="strona.php">Strona</a>
          <br>
-         <a href="daneDoBazy.html">Dane Do Bazy</a>
+         <a href="DaneBaza.html">Dane Do Bazy</a>
          <br>
 	 <a href="insert.php">insert.php</a>
     </div>
