45edb8bc4371c5936e9b8be07d1043d07f1caa84
Dawid Szafrański <73634614+szafranski-dawid@users.noreply.github.com>
2020-11-26 08:25:30 +0100

    Create nieobecnosci.php

---
+++ nieobecnosci.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <h1>Filip Jabłoński</h1>
+<nav>
+    <br>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-szafranski-dawid">github</a>
+    <br>
+    <br>
+    <div class="nav">
+         <a href="index.php">Strona Główna</a>  
+         <a href="pracownicy.php">Pracownicy</a>
+         <a href="pracownicy_organizacja.php">Pracownicy i Organizacja</a>   
+         <a href="funkcje_agregujace.php">Funkcje Agregujace</a>  
+         <a href="data_czas.php">Data i Czas</a>
+         <a href="nieobecnosci.php">Nieobecności Pracowników</a>
+         <a href="strona.php">Strona</a>
+         <a href="daneDoBazy.html">Dane Do Bazy</a>
+         <a href="insert.php">insert.php</a>
+    </div>
+    <br>
+</nav>  
+<?php
+?>
+</body>
+</html>
