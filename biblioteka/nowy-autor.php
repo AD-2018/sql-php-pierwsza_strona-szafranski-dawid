c1a3a9c86021900796f6f4bc4d1d05dfcd7fd8af
Dawid Szafrański <73634614+szafranski-dawid@users.noreply.github.com>
2021-02-08 07:57:21 +0100

    Create nowy-autor.php

---
+++ biblioteka/nowy-autor.php
@@ -0,0 +1,29 @@
+<?php
+echo("jestes w new_autor.php <br>");
+echo "<li>". $_POST['autor'];
+
+$servername = "mysql-kcz.alwaysdata.net";
+$username = "kcz";
+$password = "zaq1@WSX";
+$dbname = "kcz_20";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "INSERT INTO bibl_autor (id_autor,autor) 
+      VALUES (null, '".$_POST['autor']."')";
+
+//wyświetlamy zapytanie $sql
+echo "<li>". $sql;
+
+if ($conn->query($sql) === TRUE) {
+   header ('Location:https://kozak-szymon.herokuapp.com/Biblioteka/biblioteka.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
