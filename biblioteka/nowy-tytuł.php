cd92b5790eb4b9e4256493429469618a028f9d1e
Dawid Szafrański <73634614+szafranski-dawid@users.noreply.github.com>
2021-02-08 07:58:03 +0100

    Create nowy-tytuł.php

---
+++ biblioteka/nowy-tytuł.php
@@ -0,0 +1,29 @@
+<?php
+echo("jestes w new_tytul.php <br>");
+echo "<li>". $_POST['tytul'];
+
+$servername = "mysql-kcz.alwaysdata.net";
+$username = "kcz";
+$password = "zaq1@WSX";
+$dbname = "kcz_20";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "INSERT INTO bibl_tytul (id_tytul,tytul) 
+       VALUES (null, '".$_POST['tytul']."')";
+
+//wyświetlamy zapytanie $sql
+echo "<li>". $sql;
+
+if ($conn->query($sql) === TRUE) {
+   header ('Location:https://kozak-szymon.herokuapp.com/Biblioteka/biblioteka.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
