b9fceb87302d1b77b8f10efd13eae9a8ae47aa83
Dawid Szafrański <73634614+szafranski-dawid@users.noreply.github.com>
2021-03-04 11:35:04 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,12 +1,8 @@
 <?php
-$servername = "mysql-kcz.alwaysdata.net";
-$username = "kcz";
-$password = "zaq1@WSX";
-$dbname = "kcz_20";
+$servername = $_SERVER["servername"];
+$username = $_SERVER["username"];
+$password = $_SERVER["password"];
+$dbname = $_SERVER["dbname"];
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+$conn = new mysqli ($servername, $username, $password, $dbname);
 ?>
